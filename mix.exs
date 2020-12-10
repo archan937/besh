39844a4c12620a50005a59f58d6d0fca2bbfed3e
Paul Engel <pm_engel@icloud.com>
2020-12-10 20:48:40 +0100

    Add excoveralls and package information

--- mix.exs
+++ mix.exs
@@ -7,10 +7,41 @@ defmodule Besh.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      name: "Besh",
+      description: description(),
+      deps: deps(),
+      package: package(),
       escript: [
         main_module: Besh.CLI,
         path: "bin/besh"
-      ]
+      ],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      test_coverage: [tool: ExCoveralls]
+    ]
+  end
+
+  defp description do
+    """
+    Transpile Elixir scripts to Bash scripts
+    """
+  end
+
+  defp deps do
+    [
+      {:excoveralls, "~> 0.12.3", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Paul Engel"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/archan937/besh"}
     ]
   end
 end
